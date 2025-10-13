78f48990d44452ddc9b2ccbc9e8289cf20458223
Elshadai Veloso <150169442+Elshadaiv@users.noreply.github.com>
2025-10-13 15:54:23 +0100

    updated login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,24 +1,83 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 
 
 
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 import 'package:tinytales/components/my_button.dart';
 import 'package:tinytales/components/my_textfield.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
-void homePage()
-{
+class LoginPage extends StatefulWidget {
+  final Function()? onTap;
+  const LoginPage({super.key, required this.onTap});
 
+  @override
+  State<LoginPage> createState() => LoginPageState();
 }
+
+class LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void signIn() async
+  {
+
+    final BuildContext stateContext = this.context;
+    showDialog(
+      context: stateContext,
+      builder: (BuildContext dialogContext)
+      {
+        return const Center(
+          child:  CircularProgressIndicator(),
+        );
+      },
+    );
+
+
+
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+      Navigator.pop(stateContext);
+    } on FirebaseAuthException catch (e) {
+      Navigator.pop(stateContext);
+      showErrorMessage(e.code);
+      }
+    }
+
+  void showErrorMessage(String message) {
+    final BuildContext stateContext = this.context;
+
+    showDialog(
+      context: stateContext,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          backgroundColor: Colors.amber,
+          title: Center(
+            child: Text(
+              message,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Center(
+          child: SingleChildScrollView(
           child: Column(
             children: [
               const SizedBox(height: 50),
@@ -42,6 +101,7 @@ void homePage()
 
               // Username TextField
               MyTextField(
+                controller: emailController,
                 hintText: 'Email',
                 obscureText: false,
                 ),
@@ -50,6 +110,7 @@ void homePage()
 
               //PASSWORD TEXT FIELD
               MyTextField(
+                controller: passwordController,
                 hintText: 'Password',
                 obscureText: true,
 
@@ -57,7 +118,8 @@ void homePage()
               const SizedBox(height: 25),
 
               MyButton(
-              onTap: homePage,
+                text: 'Sign in',
+              onTap: signIn,
               ),
 
               const SizedBox(height: 25),
@@ -71,11 +133,14 @@ void homePage()
                   ),
 
                   const SizedBox(width: 4,),
-                  const Text(
-                    'Register Here',
-                        style: TextStyle(
-                          color:  Colors.blueAccent, fontWeight:FontWeight.bold
-                        ),
+                  GestureDetector(
+                    onTap: widget.onTap,
+                    child: const Text(
+                      'Register Here',
+                          style: TextStyle(
+                            color:  Colors.blueAccent, fontWeight:FontWeight.bold
+                          ),
+                    ),
                   ),
                 ],
 
@@ -84,6 +149,8 @@ void homePage()
           ),
         ),
       ),
+      )
     );
   }
 }
+
