e2521cb420195d6e01fab65d8018cfc812872046
elshhh <c22388081@mytudublin.ie>
2025-10-14 13:34:48 +0100

    These commits had to be done as i had my git completly wrong i now fixed everything

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -15,7 +15,7 @@ class MyButton extends StatelessWidget {
       padding: const EdgeInsets.all(25),
       margin: const EdgeInsets.symmetric(horizontal: 25),
         decoration: BoxDecoration(
-          color: Colors.green,
+          color: Colors.pinkAccent,
             borderRadius: BorderRadius. circular(8),
         ),
       child: Center(child: Text(text,
