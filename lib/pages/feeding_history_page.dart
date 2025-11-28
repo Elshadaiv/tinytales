4f838a097d77ee4afaaee2e25c1fde2281daa003
elshhh <c22388081@mytudublin.ie>
2025-11-28 15:02:25 +0000

    this page is made to track the bottle feeding history saved in the database and is printed out for the user

---
+++ lib/pages/feeding_history_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:tinytales/services//feedingHistoryList.dart';
+
+class FeedingHistoryPage extends StatelessWidget {
+  final String babyId;
+
+  const FeedingHistoryPage({super.key, required this.babyId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey[300],
+        title: const Text("Feeding History"),
+      ),
+
+      backgroundColor: Colors.grey[300],
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: FeedingHistoryList(babyId: babyId),
+      ),
+    );
+  }
+}
