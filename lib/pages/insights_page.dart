5691a28135f7948d1669868626acac94bf27301f
elshhh <c22388081@mytudublin.ie>
2026-02-02 17:59:41 +0000

    insights page the bare bones this is whats going to be used for the machine learning. its also getitng the data from the cry detection class tooo. this insights page at the moment is just reading a test spetrogram image that wasnt put in the dataset and is being put thru process of the image being read, this is caused by the cry detection class if the interpter running and reads the image itll give the top 2 results this shows that the machine learning is reading the images the waves thru cnn and classifying the cries in the label dataset i want the ui something similar to this and incorporate the live microphone so its just not reading the 1 image but creating one on the spot for it to be analysed and classifed this a hardcoded demo that shows the proof of concept

--- lib/pages/insights_page.dart
+++ lib/pages/insights_page.dart
@@ -1,27 +1,159 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:tinytales/services/cry_detection.dart';
 
-class InsightsPage extends StatefulWidget {
+
+class InsightsPage extends StatefulWidget
+{
   const InsightsPage({super.key});
 
   @override
   State<InsightsPage> createState() => _InsightsPageState();
 }
 
-class _InsightsPageState extends State<InsightsPage> {
+class _InsightsPageState extends State<InsightsPage>
+{
+  bool isRunning = false;
+
+  String title = "Ready";
+  String body = "Tap to test cry detection";
+  final String demoAssetPath = "assets/machineLearning/test_spectrogram.png";
+
+  Future<void> _runTest() async
+  {
+    if (isRunning)
+    {
+      return;
+    }
+
+    setState(()
+    {
+      isRunning = true;
+      title = "Listening";
+      body= "Testing cry detection";
+    });
+
+    try
+    {
+      final result = await cry_Detection().predictCryFromAsset(demoAssetPath);
+
+      if (!mounted)
+      {
+        return;
+      }
+
+      setState(()
+      {
+        isRunning = false;
+        title = "Result";
+        body = result.toString();
+      });
+    }
+    catch (e)
+    {
+      if (!mounted)
+      {
+        return;
+      }
+
+      setState(()
+      {
+        isRunning = false;
+        title = "Error";
+        body = e.toString();
+      });
+    }
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
+    final Color background = Colors.grey.shade200;
+    final Color card = Colors.white;
+    final Color accent = Colors.grey.shade900;
 
-    final user = FirebaseAuth.instance.currentUser;
+    return Scaffold(
+      backgroundColor: background,
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: 24),
+            Text(
+              "Cry Detection",
+              style: TextStyle(fontSize: 22,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
 
-    return  Scaffold(
-        backgroundColor: Colors.grey[300],
-        appBar: AppBar(
-          backgroundColor: Colors.grey[300],
+            SizedBox(height: 10),
+            Expanded(
+              child: Center(
+                child: GestureDetector(
+                  onTap: _runTest,
+                  child: AnimatedContainer(
+                    duration: Duration(milliseconds: 200),
+                    width: isRunning ? 180 : 200,
+                    height: isRunning ? 180 : 200,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: card,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 20,
+                          offset: Offset(0, 10),
+                        ),
+                      ],
+                    ),
+                    alignment: Alignment.center,
+                    child: Icon(
+                      isRunning ? Icons.graphic_eq : Icons.mic,
+                      size: 48,
+                      color: accent,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.fromLTRB(18, 0, 18, 18),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: card,
+                borderRadius: BorderRadius.circular(18),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 8,
+                    offset: Offset(0, 4),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children:[
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                  SizedBox(height: 6),
+                  Text(
+                    body,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.black54,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-       body:  Center(
-          child: Text('Insight Page ' + (FirebaseAuth.instance.currentUser?.email ?? 'Unknown')),
-        ),
+      ),
     );
   }
 }
