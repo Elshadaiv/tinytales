f42105e60481965d63970fb2c61890693a81f71b
elshhh <c22388081@mytudublin.ie>
2026-02-02 17:10:24 +0000

    cry detection functionn used for my machine leanring its getting the train model that i created and exported which holds the trained dataset of differnet cries and categories them this class is basically taking the spetrogram image as an input resizes it runs the model locally and gives the top 2 predicted cry it could be for the cry i will incorporate live microphone as it is reading off a pre set cry to see if the function is working which it is but at a lower confidence still works.

---
+++ lib/services/cry_Detection.dart
@@ -0,0 +1,135 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/services.dart';
+import 'package:image/image.dart' as img;
+import 'package:path_provider/path_provider.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
+
+class cry_Detection
+{
+  static final cry_Detection _instance = cry_Detection._internal();
+  factory cry_Detection() => _instance;
+  cry_Detection._internal();
+
+  Interpreter? _interpreter;
+  List<String> _labels = [];
+  final int inputSize = 128;
+
+  Future<void> loadModel() async
+  {
+    if (_interpreter != null)
+    {
+      return;
+    }
+
+    final ByteData modelData = await rootBundle.load("assets/machineLearning/baby_sound_classifier_v4.tflite");
+
+    final Uint8List modelBytes = modelData.buffer.asUint8List(modelData.offsetInBytes, modelData.lengthInBytes);
+
+    _interpreter = Interpreter.fromBuffer(
+      modelBytes,
+      options: InterpreterOptions()
+        ..threads = 2,
+    );
+
+    final rawLabels = await rootBundle.loadString("assets/machineLearning/labels.txt",
+    );
+
+    _labels = rawLabels
+        .split("\n")
+        .map((e) => e.trim())
+        .where((e) => e.isNotEmpty)
+        .toList();
+  }
+  Future<File> _assetToTempFile(String assetPath) async
+  {
+    final ByteData data = await rootBundle.load(assetPath);
+    final Directory tempDir = await getTemporaryDirectory();
+
+    final String ext = assetPath.toLowerCase().endsWith(".jpg") || assetPath.toLowerCase().endsWith(".jpeg")
+        ? ".jpg" : ".png";
+
+    final File file = File("${tempDir.path}/spectrogram_${DateTime.now().millisecondsSinceEpoch}$ext");
+    await file.writeAsBytes(data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes),
+    );
+
+    return file;
+  }
+
+
+  Future<String> predictCryFromAsset(String assetPath) async
+  {
+    final File tempFile = await _assetToTempFile(assetPath);
+    return predictCry(tempFile);
+  }
+
+
+  Future<String> predictCry(File spectrogramImage) async
+  {
+    if (_interpreter == null)
+    {
+      await loadModel();
+    }
+
+    final Uint8List bytes = await spectrogramImage.readAsBytes();
+    final img.Image? image = img.decodeImage(bytes);
+
+    if (image == null)
+    {
+      throw Exception("Error");
+    }
+
+    final resized = img.copyResize(
+      image,
+      width: inputSize,
+      height: inputSize,
+      interpolation: img.Interpolation.average,
+    );
+
+    final input = List.generate(
+      1,
+          (_) => List.generate(
+        inputSize,
+            (y) => List.generate(
+          inputSize,
+              (x)
+          {
+            final pixel = resized.getPixel(x, y);
+            final r = pixel.r / 255.0;
+            final g = pixel.g / 255.0;
+            final b = pixel.b / 255.0;
+            final gray = (0.299 * r + 0.587 * g + 0.114 * b);
+            return [gray, gray, gray];
+          },
+        ),
+      ),
+    );
+
+    final int numClasses = _interpreter!.getOutputTensor(0).shape[1];
+    final output = List.generate(1, (_) => List.filled(numClasses, 0.0),
+    );
+    _interpreter!.run(input, output);
+
+    final prediction = output[0];
+
+    final pairs = List.generate(prediction.length, (i) => {"i": i, "s": prediction[i]},
+    );
+
+    pairs.sort((a, b) => (b["s"] as double).compareTo(a["s"] as double));
+
+    final top2 = pairs.take(2).toList();
+    final int i0 = top2[0]["i"] as int;
+    final double s0 = top2[0]["s"] as double;
+    final String l0 = i0 < _labels.length ? _labels[i0] : "/";
+    final int p0 = (s0 * 100).round();
+
+    final int i1 = top2[1]["i"] as int;
+    final double s1 = top2[1]["s"] as double;
+    final String l1 = i1 < _labels.length ? _labels[i1] : "/";
+    final int p1 = (s1 * 100).round();
+
+
+    return "$l0 ($p0%), $l1 ($p1%)";
+  }
+}
