269e9ed8c6d8cc8c5864bd68d3c474412e4010ed
elshhh <c22388081@mytudublin.ie>
2026-03-29 15:42:58 +0100

    updated dependcies for to get maps included for the community section community_info class will show the event that the user interacts on the map it will show picutre event name and venue and the date and a view event button that which will bring them to eventbrite or website to book tickets for the event soon will be doing api claling and intergrading the map

---
+++ lib/services/community_info_card.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'community_event.dart';
+
+class CommunityInfoCard extends StatelessWidget
+{
+  final CommunityEvent event;
+  final VoidCallback onViewEvent;
+
+  CommunityInfoCard(
+      {
+    super.key,
+    required this.event,
+    required this.onViewEvent,
+  });
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Container(
+      margin: EdgeInsets.all(12), padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: Colors.white, borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12, blurRadius: 8, offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: event.imageUrl.isNotEmpty
+                ? Image.network(event.imageUrl, width: 90, height: 90,
+              fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace)
+              {
+                return Container(
+                  width: 90, height: 90,
+                  color: Colors.grey[300],
+                  child: Icon(
+                    Icons.image_not_supported,
+                    color: Colors.grey[700],
+                  ),
+                );
+              },
+            )
+                : Container(
+              width: 85, height: 85,
+              color: Colors.grey[300],
+              child: Icon(
+                Icons.image,
+                color: Colors.grey[700],
+              ),
+            ),
+          ),
+          SizedBox(width: 12),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  event.title,
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold,),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(height: 6),
+                Text(
+                  DateFormat('EEE, d MMM • HH:mm').format(event.startDateTime),
+                  style: TextStyle(
+                    fontSize: 13,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                SizedBox(height: 4),
+                Text(
+                  event.venueName,
+                  style: TextStyle(fontSize: 13, color: Colors.grey[700],
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(height: 8),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: onViewEvent,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.purple,
+                      foregroundColor: Colors.white,
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Text("View Event"),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
