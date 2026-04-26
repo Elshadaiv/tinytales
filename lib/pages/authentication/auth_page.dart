d936e8c8187e511c6958d71c51906d408f793292
elshhh <c22388081@mytudublin.ie>
2026-04-26 18:59:07 +0100

    added when the user first logs in for the first time that the it brings them onto the profile page to create a new baby once theyve made a baby log out and log back in that it goes back to the home page just better app flow

--- lib/pages/authentication/auth_page.dart
+++ lib/pages/authentication/auth_page.dart
@@ -1,8 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tinytales/pages/main/home_page.dart';
 import 'package:tinytales/pages/authentication/login_or_register_page.dart';
 import 'package:tinytales/pages/authentication/login_page.dart';
+import 'package:tinytales/pages/main/profile_page.dart';
+
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
@@ -15,9 +18,33 @@ class AuthPage extends StatelessWidget {
         builder: (context, snapshot)
           {
             if(snapshot.hasData)
-              {
-                return HomePage();
-              }
+            {
+              final userId = snapshot.data!.uid;
+
+              return FutureBuilder(
+                future: FirebaseFirestore.instance
+                    .collection("baby_profiles")
+                    .where("userId", isEqualTo: userId)
+                    .limit(1)
+                    .get(),
+                builder: (context, babySnapshot)
+                {
+                  if (babySnapshot.connectionState == ConnectionState.waiting)
+                  {
+                    return Center(
+                      child: CircularProgressIndicator(color: Colors.purple),
+                    );
+                  }
+
+                  if (babySnapshot.hasData && babySnapshot.data!.docs.isEmpty)
+                  {
+                    return ProfilePage();
+                  }
+
+                  return HomePage();
+                },
+              );
+            }
 
             //not logged in
             else
